3add99d8370ae3f8d1b85603ea8eac54e90758cf
Batalawata <89070748+EmilHaqvinsson@users.noreply.github.com>
2022-03-13 17:38:11 +0100

    pre 0ad css på getAll

--- frontend/src/utils/api/service/AliveService.js
+++
@@ -1,9 +0,0 @@
-import http from '../UserApi'
-
-const alive = () => {
-    return http.get('/')
-}
-
-export default {
-    alive
-}
--- rebuild/src/views/NewItem.js
+++ rebuild/src/views/NewItem.js
@@ -1,14 +1,44 @@
-import { Link } from 'react-router-dom'
-import RoutingPath from '../router/RoutingPath'
+import {useState} from 'react'
+import {MdDoneOutline, MdDone} from 'react-icons/md'
+import classes from './NewItem.module.css'
 
 const NewItem = () => {
-    
+const [title, setTitle] = useState('')
+const [deadline, setDeadline] = useState('')
+const [text, setText] = useState('')
+const [assignedTo, setAssignedTo] = useState('')
 
-    return (
+const handleChangedTitle = (e)=> {
+    setTitle(e.target.value)
+}
         <>
             <h1>ADD NEW ITEM</h1>
+            <form class="pa4 black-80">
+                <div class="measure">
+                    <label for="title" class="f6 b db mb2">
+                        <span class="normal black-60"></span>
+                    </label>
+                    <input id="title"
+                           class="input-reset ba b--black-20 pa2 mb2 db w-100"
+                           type="text"
+                           placeholder={'Joe Giggolo'}
+                           aria-describedby="name-desc">
+    <small id="name-desc" class="f6 black-60 db mb2">Apparently helps with Accessibility</small></input>
+    </div>
+                <div className="measure">
+                    <label htmlFor="deadline" className="f6 b db mb2">
+                        <span className="normal black-60"></span>
+                    </label>
+                    <input id="title"
+                           className="input-reset ba b--black-20 pa2 mb2 db w-100"
+                           type="date"
+                           aria-describedby="name-desc">
+                        <small id="name-desc" className="f6 black-60 db mb2">Please set a deadline for your item.</small></input>
+                </div>
+</form>
+            <h1><MdDoneOutline/><MdDone/></h1>
         </>
     );
-};
+}}
 
 export default NewItem;
